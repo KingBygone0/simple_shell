ff2a31de8c0a620bc997991d064621665e44d9f1
NanaOdamme <eronastyles@gmail.com>
2023-09-18 23:38:43 -0700

    replaced

--- shell.h
+++ shell.h
@@ -2,45 +2,63 @@
 #define SHELL_H
 
 #include <stdio.h>
+
 #include <stdlib.h>
 #include <unistd.h>
-#include <sys/wait.h>
-#include <stdbool.h>
-#include <errno.h>
-#include <stddef.h>
-#include <stddef.h>
 #include <string.h>
-
-
-#define MAX_LEN 100
-#define M_ARG 30
+#include <sys/wait.h>
+#include <errno.h>
+#define MAX_ENV_VARS 100
+#define MAX_INPUT_LENGTH 1024
+extern int num_env_vars;
 extern char **environ;
-void execute_commands_from_file(const char *filename);
-/* printing commands*/
-void my_putchar(char c);
-int my_puts(const char *string);
-void _tokenize(char *input, char *tokens[], int *count);
-void execute_command(char *command, char *arguments[]);
-char *is_executable(char *command);
-char *concat_strings(const char *a1, const char *a2);
-char *my_path(const char *command);
-void _environment(void);
-size_t my_strcspn(const char *str, const char *str2);
-void copy_string(char *destination, const char *source);
-char *duplicate(const char *string);
-char *concat(char *dt, const char *source);
-int compare(const char *a1, const char *a2);
-int string_cmp(const char *a1, const char *a2, size_t b);
-void copy_string(char *destination, const char *source);
-char *sub_string(const char *longstring, const char *single);
-size_t string_len(const char *w);
-char *my_strchr(const char *str, int character);
-size_t my_strspn(const char *str, const char *accept);
-char *my_strndup(const char *str, size_t n);
-void split_commands(char *input, char *commands[], int *num_commands);
-void execute_exit(char *args[]);
-void execute_commands(const char *input);
-void execute_logical_operators(const char *input);
-int my_Atoi(const char *string);
-#endif
+/**
+ * struct EnvVar - Structure to represent an environment variable.
+ * @name: The name of the environment variable.
+ * @value: The value associated with the environment variable.
+ */
+struct EnvVar
+{
+	char *name;
+	char *value;
+};
 
+void strip_comments(char *input);
+void execute_setenv(const char *variable,
+		const char *value);
+void execute_unsetenv(const char *variable, const char *program_name);
+int my_setenv(const char *name, const char *value, int overwrite);
+int my_unsetenv(const char *name);
+char *int_to_string(int num);
+void display_prompt(void);
+void execute_exit(char **args);
+void execute_env(void);
+int execute_command(const char *program_name,
+		const char *command, char *args[]);
+void tokenize_input_line(char *input, char **command,
+		char *args[], int *num_args);
+void execute_command_in_path(const char *program_name,
+		const char *command, char *args[]);
+int read_input_line(char **input, size_t *input_length,
+		const char *program_name);
+void execute_cd(char *args[]);
+void copy_string(char *destination, const char *source);
+int compare(const char *a1, const char *a2);
+int my_Atoi(const char *string);
+size_t my_strcspn(const char *str, const char *str2);
+size_t string_len(const char *w);
+char *sub_string(const char *longstring, const char *single);
+int string_cmp(const char *a1, const char *a2, size_t b);
+char *concat(char *dt, const char *source);
+char *duplicate(const char *string);
+void execute_individual_command(char *program_name,
+		char *command, char *args[]);
+char *handle_variables(const char *input,
+		const char *program_name, pid_t pid);
+char *my_path(const char *command);
+void process_command(const char *command, char **args,
+		int num_args, const char *program_name);
+char *my_strchr(const char *str, int c);
+char *my_strncpy(char *dest, const char *src, size_t n);
+char *my_strstr(const char *haystack, const char *needle);
+#endif
